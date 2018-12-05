38bbcd846e03238eb95f1a2ac0ab17d113d0a61a
Rasmus Johansson <racemouse95@gmail.com>
2018-12-05 23:42:56 +0100

    comments for plan

--- day3.py
+++ day3.py
@@ -4,3 +4,6 @@ for x in range(0,len(input)):
     input[x] = input[x][0:-1]
 
 
+#1. check boundaries of the rectangles(coords) and see if they overlap. First in X, then in Y if they do overlap in X
+#2. just mark the area as overlapped and move on to #1. (dont wanna calculate the same overlapped area twice)
+#3. when all are checked, calculate the overlapped area
